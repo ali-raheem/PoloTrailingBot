f9a57a55f5165a1b1ac12b978c7cac69df13ef76
Ali Raheem <ali.raheem@gmail.com>
2018-01-11 01:58:34 +0000

    Initial commit

---
+++ ptb.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+# Get prices for pair
+
+import urllib2, json
+from tradingApi import polo
+
+class Order:
+	def __init__(self, api, pair, amount, offset, sell = True, fixed = True):
+		self.api = api
+		self.pair = pair
+		self.amount = float(amount)
+		self.sell = sell
+		self.offset = float(offset)
+		self.fixed = fixed
+   		self.rate = 0
+		self.getRate()
+		self.price = self.rate
+		self.done = 0
+	def getRate(self):
+   		pair = self.api.prices[self.pair]
+		if self.sell:
+			self.rate = float(pair["highestBid"])
+		else:
+			self.rate = float(pair["lowestAsk"])
+
+	def updatePrice(self):
+   		pair = self.api.prices[self.pair]
+		if self.sell:
+			self.price = (self.price + float(pair["highestBid"]))/2
+		else:
+			self.price = (self.price + float(pair["lowestAsk"]))/2
+
+def checkOrder(order):
+	if order.sell:
+		if(order.fixed):
+			thres = order.rate - order.offset
+		else:
+			thres = order.rate - order.rate * order.offset/100
+		if order.price > order.rate:
+			order.rate = order.price
+			print("{}: New peak rate: {}".format(order.pair, order.price))
+		elif (order.price < thres):
+			order.done = True
+			#api.sell(order.pair, order.amount, order.price)
+			print("sell {} on {} at {}.".format(order.amount, order.pair, order.price))
+		print("{} is at {} will order at {}".format(order.pair, order.price, thres))
+	else:
+		if(order.fixed):
+			thres = order.rate + order.offset
+		else:
+			thres = order.rate + order.rate * order.offset/100
+		if order.price < order.rate:
+			order.rate = order.price
+			print("{}: New peak rate: {}".format(order.pair, order.price))
+		elif (order.price > thres):
+			order.done = True
+			#api.buy(order.pair, order.amount, order.price)
+			print("buy {} on {} at {}.".format(order.amount, order.pair, order.price))
+		print("{} is at {} will order at {}".format(order.pair, order.price, thres))
+
+
+if "__main__" == __name__:
+	config = json.loads(open('config.json', 'r').read())
+	api = polo(config["api-key"], config["api-secret"])
+	api.getPrices()
+	orders = []
+	order = Order(api, "BTC_DOGE", 100, 5, sell = True, fixed = False)
+	order.updatePrice()
+	orders.append(order)
+	while True:
+		api.getPrices()
+		for order in orders:
+			order.updatePrice()
+			if order.done:
+				orders.remove(order)
+				break
+			checkOrder(order)
+
---
+++ tradingApi.py
@@ -0,0 +1,69 @@
+import urllib2, json
+import requests
+import hmac
+import hashlib
+from itertools import count
+import time
+
+class polo:
+	prices = {}
+	def __init__(self, key, secret):
+		self.key = key
+		self.secret = secret
+		self.NONCE_COUNTER = count(int(time.time() * 1000))
+		self.getPrices()
+		
+	def makeRequest(self, payload, headers=''):
+		payload['nonce'] = next(self.NONCE_COUNTER)
+		request = requests.Request(
+			'POST', 'https://poloniex.com/tradingApi',
+			data=payload, headers=headers
+		)
+		prepReq = request.prepare()
+		reqSig = hmac.new(self.secret, prepReq.body, digestmod=hashlib.sha512)
+		prepReq.headers['Sign'] = reqSig.hexdigest()
+		prepReq.headers['Key'] = self.key
+
+		with requests.Session() as session:
+			response = session.send(prepReq)
+			response = json.loads(response.text)
+			try:
+				print(response["error"])
+			except:
+				return response
+
+	def moveOrder(self, orderid, rate):
+		payload = {}
+		payload["command"] = "moveOrder"
+		payload["orderNumber"] = orderid
+		payload["rate"] = rate
+		self.makeRequest(payload)
+		
+	def makeOrder(self, type, currencyPair, rate, amount):
+		payload = {}
+		payload["command"] = type
+		payload["rate"] = rate
+		payload["amount"] = amount
+		payload["currencyPair"] = currencyPair
+		selfmakeRequest(payload)
+
+	def buy(self, currencyPair, rate, amount):
+		self.makeOrder("buy", currencyPair, rate, amount)
+	def sell(self, currencyPair, rate, amount):
+		self.makeOrder("sell", currencyPair, rate, amount)
+
+	def getPrices(self):
+		try:
+			reqUrl = "https://poloniex.com/public?command=returnTicker"
+			response = urllib2.urlopen(reqUrl)
+			self.prices = json.loads(response.read())
+			print("Prices updated")
+		except:
+			print("Prices update failed")
+				
+	def getBalances(self):
+		payload = {}
+		payload["command"] = "returnBalances"
+		return self.makeRequest(payload)
+
+		
